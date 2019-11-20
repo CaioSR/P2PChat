672a1205a8bae7f3995f766b5c2cb61a88215440
Caio Shimada Rabello <xcaiosr@gmail.com>
2019-11-20 09:21:45 -0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # P2PChat
 A P2P Chat developed in Python
+
+Execute the Tracker on tracker.py first
+
+Then execute chat.py and chat2.py -and chat3.py.
--- src/chat3.py
+++ src/chat3.py
@@ -2,8 +2,10 @@ from server import *
 from client import *
 import threading
 
+ip = socket.gethostbyname(socket.gethostname())
+
 sThread = threading.Thread(target=Server, args=(5003,))
 sThread.daemon = True
 sThread.start()
-client = Client(('127.0.0.1', 5003))
+client = Client((ip, 5003))
 
